d263bfe68db4212b5a5710f6a8fdcc69d4797e51
errishabhsrivastava <ersrivastavarishabh@gmail.com>
2024-06-30 16:46:42 +0530

    Upreak v1.1.1

--- config/redisClient.js
+++
@@ -1,9 +0,0 @@
-var config = rootRequire('config/config');
-const redis = require("redis");
-const redisClient = redis.createClient(config.redisDb);
-
-redisClient.on("error", function (error) {
-    console.error('Error in redis client connection, error ' + error);
-});
-
-module.exports = redisClient;
